f0aaf1f54d2f06087c00227511178c1f6d073424
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-13 23:44:58 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { FavoriteColor } from './Hooks/intro';
 
 ReactDOM.render(
     <>
-        
+        <FavoriteColor />
     </>,    
     document.getElementById("root")
 );
