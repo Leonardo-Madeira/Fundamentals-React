306103a712c6991015710f97ac7c6c018c8da34b
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-14 22:08:35 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { FavoriteColor } from './Hooks/intro';
+import { Counter, Timer, Timer3 } from './Hooks/UseEffect';
 
 ReactDOM.render(
     <>
         <FavoriteColor />
+        <Timer />
+        <Counter />
     </>,    
     document.getElementById("root")
 );
